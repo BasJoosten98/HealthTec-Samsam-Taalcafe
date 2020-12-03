48a7f46e0c435cf675e5f7f83d28d3289f8f2a28
Marco <marco.jacobs@student.fontys.nl>
2020-12-03 12:24:19 +0100

    Added models

---
+++ TaalcafeMVC/Taalcafe/Taalcafe/Models/Sessie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taalcafe.Models
+{
+    public class Sessie
+    {
+        #region Properties
+        public int Id { get; set; }
+        public DateTime Datum { get; set; }
+        public int ThemaId { get; set; }
+        #endregion
+    }
+}
---
+++ TaalcafeMVC/Taalcafe/Taalcafe/Models/SessiePartner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taalcafe.Models
+{
+    public class SessiePartner
+    {
+        #region Properties
+        public int TaalcoachId { get; set; }
+        public int CursistId { get; set; }
+        public int SessieId { get; set; }
+        public string FeedbackTaalcoach { get; set; }
+        public string FeedbackCursist { get; set; }
+        public int CijferTaalcoach { get; set; }
+        public int CijferCursist { get; set; }
+        #endregion
+    }
+}
