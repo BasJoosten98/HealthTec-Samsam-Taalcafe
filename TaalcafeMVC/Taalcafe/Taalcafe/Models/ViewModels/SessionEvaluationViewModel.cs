3bcc104cb824d29aa8581fcaa0765e03a2906d23
JasperBlok <46027465+JasperBlok@users.noreply.github.com>
2021-01-20 16:03:45 +0100

    Created view and viewmodel for session evaluation. (no controller actions)

---
+++ TaalcafeMVC/Taalcafe/Taalcafe/Models/ViewModels/SessionEvaluationViewModel.cs
@@ -0,0 +1,10 @@
+namespace Taalcafe.Models.ViewModels
+{
+    public class SessionEvaluationViewModel
+    {
+        public int SessieId { get; set; }
+        public int GebruikersId { get; set; }
+        public string Feedback { get; set; }
+        public int Cijfer { get; set; }
+    }
+}
